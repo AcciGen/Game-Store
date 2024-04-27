8d7f82a590d19922f685910f09418dc0019adf51
AcciGen <145344775+AcciGen@users.noreply.github.com>
2024-04-27 08:02:56 +0500

    Add project files.

---
+++ Game-Store/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Game_Store.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
