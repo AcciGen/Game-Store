f6583562aad843004a046375a0eea676b719f5aa
AkramjonA <akramzonabduvahobov6@gmail.com>
2024-04-27 11:09:55 +0500

    add getby id

--- Game-Store.Application/UseCases/Queries/GetAllGamesQuery.cs
+++ Game-Store.Application/UseCases/Queries/GetAllGamesQuery.cs
@@ -1,10 +1,6 @@
 ﻿using Game_Store.Domain.Entities;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Game_Store.Application.UseCases.Queries
 {
