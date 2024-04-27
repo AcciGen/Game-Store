3b43f793147555160a46c893b465e127286d4c53
AkramjonA <akramzonabduvahobov6@gmail.com>
2024-04-27 12:41:30 +0500

    add views and controller exceptions

---
+++ Game-Store/Controllers/ExceptionsController.cs
@@ -0,0 +1,23 @@
+﻿using Game_Store.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Game_Store.Controllers
+{
+    public class ExceptionsController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Error(string message, int code)
+        {
+            ExceptionViewModel model = new ExceptionViewModel()
+            {
+                Code = code,
+                Message = message
+            };
+            return View(model);
+        }
+        public IActionResult NotFound()
+        {
+            return View();
+        }
+    }
+}
