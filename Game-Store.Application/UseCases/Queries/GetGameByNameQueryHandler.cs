ca0a45a758a2a6df3e90442a0f77ffb909f83383
AkramjonA <akramzonabduvahobov6@gmail.com>
2024-04-27 11:41:10 +0500

    close

--- Game-Store.Application/UseCases/Queries/GetGameByNameQueryHandler.cs
+++ Game-Store.Application/UseCases/Queries/GetGameByNameQueryHandler.cs
@@ -22,7 +22,7 @@ namespace Game_Store.Application.UseCases.Queries
                 throw new NotFoundException("Game not found");
             }
             else
-            {
+            { 
                 return game;
             }
 
