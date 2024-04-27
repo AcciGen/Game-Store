1e62302c379ec17dad08ed55b5e66635bb2f4bc0
AkramjonA <akramzonabduvahobov6@gmail.com>
2024-04-27 11:22:19 +0500

    Add Get by name query

---
+++ Game-Store.Application/UseCases/Queries/GetGameByNameQuery.cs
@@ -0,0 +1,10 @@
+﻿using Game_Store.Domain.Entities;
+using MediatR;
+
+namespace Game_Store.Application.UseCases.Queries
+{
+    public class GetGameByNameQuery:IRequest<Game>
+    {
+        public string GameName { get; set; }
+    }
+}
